3b9868bcdf93b25dba5a45ad8f6680a7d538cd24
Jorge Arias <96737213+JKesari86@users.noreply.github.com>
2024-09-05 01:07:00 +0000

    update

--- src/app.js
+++ src/app.js
@@ -1,38 +1,35 @@
 window.addEventListener("load", () => {
-  let values=[
-      "A",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "J",
-      "Q",
-      "K",
+  let values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
   ];
-  
-  let symbol=[
-      "♠", "♥", "♦","♣"
-  ];
-  let colors = ["red","green"]
-  
+
+  let symbol = ["♠", "♥", "♦", "♣"];
+  let colors = ["red", "green"];
+
   let cardSymbolHeader = document.getElementById("card-symbol-header");
   let cardValue = document.getElementById("card-value");
   let cardSymbolFooter = document.getElementById("card-symbol-footer");
-  
-  let randomValues = Math.floor(Math.random()* values.length);
-  let randomSymbol = Math.floor(Math.random()* symbol.length);
-  let randomColors = Math.floor(Math.random()* colors.length);
-  
+
+  let randomValues = Math.floor(Math.random() * values.length);
+  let randomSymbol = Math.floor(Math.random() * symbol.length);
+  let randomColors = Math.floor(Math.random() * colors.length);
+
   cardSymbolHeader.style.color = colors[randomColors];
-  cardSymbolHeader.innerHTML =`<p>${symbol[randomSymbol]}</p>`;
+  cardSymbolHeader.innerHTML = `<p>${symbol[randomSymbol]}</p>`;
   cardValue.style.color = "black";
   cardValue.innerHTML = `<p>${values[randomValues]}</p>`;
   cardSymbolFooter.style.color = colors[randomColors];
   cardSymbolFooter.innerHTML = `<p>${symbol[randomSymbol]}</p>`;
-  
-  });
+});
